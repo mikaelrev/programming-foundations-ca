72768ca89c50fca7f87dddbe0db88fbe28d4db1b
mikaelrev <mikael.myrvang@gmail.com>
2021-06-11 11:52:50 +0200

    Update script.js

---
+++ js/script.js
@@ -0,0 +1,54 @@
+// Question 1
+
+var outOfStock = true;
+
+if (outOfStock === true) {
+    console.log("Out of stock")
+} else {
+    console.log("In stock")
+}
+
+
+
+// Question 2
+
+for (var i = 15; i <= 25; i++) {
+    if (i === 17 || i === 20) {
+        console.log(i);
+    }
+}
+
+
+
+// Question 3
+
+var games = [
+	{
+		title: "Grand Theft Auto",
+		rating: 4.48,
+	},
+	{
+		title: "Portal 2",
+		rating: 3.5,
+	},
+	{
+		title: "Team Fortress",
+		rating: null,
+	},
+	{
+		title: "The Witcher",
+		rating: 3.0,
+	},
+	{
+		title: "The Elder Scrolls",
+		rating: 2.9,
+	},
+];
+
+var gameContainer = document.querySelector(".games");
+
+for (var i = 0; i < games.rating; i++) {
+    if (games.rating < 3.5) {
+        gameContainer.innerHTML = "<li>" + games.title + " - " + games.rating + "</li>";
+    }
+}
